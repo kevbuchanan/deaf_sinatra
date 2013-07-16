fac9b33a513ab634a2f87e69148f9f523bf451fc
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 11:13:16 -0500

    added hide and show for grandma paragraph

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  $('#grandma').hide();
   $('#say-it').submit(function(event) {
     event.preventDefault();
     var data = $(this).serialize();
     $.post('/grandma', data, function(response){
+      $('#grandma').show();
       $("#grandma_says").replaceWith($(response).find("#grandma_says"));
     })
   })
