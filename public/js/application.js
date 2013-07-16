c6d43c0a4b63b58b365cbc4f46b2c517d257ec05
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 10:57:10 -0500

    adding response

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#say-it').submit(function(event) {
     event.preventDefault();
-    
+    var data = $(this).serialize();
+    $.post('/grandma', data, function(response){
+      ;
+    })
   })
 });
